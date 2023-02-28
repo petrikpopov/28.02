ec4307f19f5ac5b0d785441f42baa1f2ac534564
petrikpopov <112963523+petrikpopov@users.noreply.github.com>
2023-02-28 13:05:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _27._02._2023
+{
+    class Program
+    {
+        static T Swap<T>(T A, T b)
+        {
+            T Temp;
+            Temp = A;
+            A = b;
+            b = Temp;
+            return A;
+        }
+        static void Main(string[] arr)
+        {
+            int Obj = Swap(6, 7);
+            Console.WriteLine(Obj);
+        }
+    }
+    
+
+}
